1b81dde6c29579d3037ea87074d567e6660bd409
Daiane567 <dr465796@gmail.com>
2021-07-23 17:47:31 -0300

    hortifruti front end

---
+++ contatos.js
@@ -0,0 +1,49 @@
+async function putData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    console.log(response)
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+var id;
+
+function vendedor() {
+
+    const id = localStorage.getItem('id');
+    let nomeVendedor = document.getElementById("nomeVendedor").value
+    let endereco = document.getElementById("endereco").value
+    let cidade = document.getElementById("cidade").value
+    let telefone = document.getElementById("telefone").value
+    let pagamento = document.getElementById("pagamento").value
+
+
+
+    putData(`http://localhost:3000/usuarioss`, {
+
+            "id": id,
+            "nomeVendedor": nomeVendedor,
+            "endereco": endereco,
+            "cidade": cidade,
+            "telefone": telefone,
+            "pagamento": pagamento
+        })
+        .then(data => {
+
+            console.log(data); // JSON data parsed by `data.json()` call
+
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+
+
+
+
+}
